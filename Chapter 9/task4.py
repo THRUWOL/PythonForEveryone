102c3bae6c784cb8021862fe40484d2265725185
Nikita Yarosh <ThRuWol@yandex.ru>
2021-12-30 14:06:05 +0300

    Create task4.py

---
+++ Chapter 9/task4.py
@@ -0,0 +1,27 @@
+def openFile(file):
+    try:
+        oFile = open(file)
+        return oFile
+    except:
+        print('Cannot open the file: ', file)
+
+
+def findMaxMessage(dictionaries, bigWord, bigCount):
+    for word, count in dictionaries.items():
+        if bigWord is None or count > bigCount:
+            bigWord = word
+            bigCount = count
+    print(bigWord, bigCount)
+
+
+file = input('Enter a file name: ')
+oFile = openFile(file)
+counts = dict()
+
+bigWord = None
+bigCount = None
+
+for line in oFile:
+    if 'From' in line and len(line.split()) > 2:
+        counts[line.split()[1]] = counts.get(line.split()[1], 0) +1
+findMaxMessage(counts, bigWord, bigCount)
