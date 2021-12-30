e9e7118f6d28c596821b607c9cf5348db784522a
Nikita Yarosh <ThRuWol@yandex.ru>
2021-12-30 13:49:48 +0300

    Create task2.py

---
+++ Chapter 9/task2.py
@@ -0,0 +1,17 @@
+def openFile(file):
+    try:
+        oFile = open(file)
+        return oFile
+    except:
+        print('Cannot open the file: ', file)
+
+
+file = input('Enter a file name: ')
+oFile = openFile(file)
+counts = dict()
+
+for line in oFile:
+    if 'From' in line and len(line.split()) > 3:
+        words = line.split()[2]
+        counts[words] = counts.get(words, 0)+1
+print(counts)
